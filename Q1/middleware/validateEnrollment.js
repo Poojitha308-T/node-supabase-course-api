81047a11a3b674d70c3b92f293285fd758388e5a
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-08 14:59:58 +0530

    First commit

---
+++ Q1/middleware/validateEnrollment.js
@@ -0,0 +1,10 @@
+export const validateEnrollment = (req, res, next) => {
+    const {student_name, course_id} = req.body;
+
+    if(!student_name || !course_id){
+        return res.status(400).json({
+            error: "These student_name and course_id fields are required"
+        });
+    }
+    next();
+};
---
+++ Q1/routes/courses.js
@@ -0,0 +1,56 @@
+import express from "express";
+import { supabase } from "./../supabaseClient";
+import { validateEnrollment } from "./../middleware/validateEnrollment";
+const router = express.Router();
+
+//get all courses
+
+router.get("/courses", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("courses").select("*");
+
+    if (error) throw error;
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// enroll student
+
+router.post("/enroll", validateEnrollment, async (req, res) => {
+  try {
+    const { student_name, course_id } = req.body;
+    const { data, error } = await supabase
+      .from("enrollments")
+      .insert([{ student_name, course_id }])
+      .select();
+
+    if (error) throw error;
+
+    res.status(201).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// get enrollments for a course
+
+router.get("/courses/:id/enrollments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { data, error } = await supabase
+      .from("enrollments")
+      .select("student_name , course_id")
+      .eq("course_id", id);
+
+    if (error) throw error;
+
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
